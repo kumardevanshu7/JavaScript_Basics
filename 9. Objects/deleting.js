09e27f9e04d8d1416a67827b9c7731517f0aee66
Devanshu <devanshukd007@gmail.com>
2023-07-02 18:51:00 +0530

    Objects : Deletion of Object Property

---
+++ 9. Objects/deleting.js
@@ -0,0 +1,11 @@
+let animal = {
+  name: "Zebra",
+  legs: 4,
+  pattern: "black & White",
+};
+
+console.log(animal.legs);
+
+// this delete word can use for removing the property of an object
+delete animal.legs;
+console.log(animal.legs);
